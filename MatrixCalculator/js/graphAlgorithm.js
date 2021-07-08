0875ddb921a908c558bbaa412c3bfae634f46aaf
Flex Alderson <63008853+LocalHost127000@users.noreply.github.com>
2021-07-08 23:29:32 +0700

    First commit

---
+++ MatrixCalculator/js/graphAlgorithm.js
@@ -0,0 +1,97 @@
+function coverTableToMatrix(tableID){
+    let table = document.getElementById(tableID)
+    let matrixResult = []
+    for (const row of table.rows) {
+        let col = []
+        for (const cell of row.cells) {
+            col.push(Number(cell.firstChild.value))
+        }
+        matrixResult.push(col)
+    }
+    return matrixResult
+}
+
+function dijkstraFalseList(shortestDistances, addedVertices, nVertices ){
+    for (let i = 0; i < nVertices; i++) {
+      shortestDistances[i] = +Infinity
+      addedVertices[i] = false;
+    }
+    return shortestDistances + addedVertices
+}
+
+function dijkstraUpdateVertices(adjacencyMatrix, shortestDistance, shortestDistances, parents, nearestVertex, nVertices, rowCells){
+    let cell = ''
+    for (let k = 0; k < nVertices; k++) {
+        let edgeDistance = adjacencyMatrix[nearestVertex][k]
+        if (edgeDistance > 0 && ((shortestDistance + edgeDistance) < shortestDistances[k])) {
+            parents[k] = nearestVertex
+            shortestDistances[k] = shortestDistance + edgeDistance
+            cell+= (`<strong> (${shortestDistances[k]},${coverNumToAlpha(k)}) </strong>`)
+        } else {
+            cell += (`(${(shortestDistances[k] == Infinity)? '&infin;' :shortestDistances[k]},${coverNumToAlpha(k)}) `)
+        }
+    }
+    rowCells.push(cell)
+    return shortestDistances + parents + rowCells
+}
+
+function dijkstra(tableID ,startVertex, tablResultID) {
+    let adjacencyMatrix = coverTableToMatrix(tableID)
+    let rowCells = []
+    let nVertices = adjacencyMatrix.length  //số đỉnh
+    let shortestDistances = new Array(nVertices) //mảng khoảng cách ngắn nhất
+    let addedVertices = new Array(nVertices)
+    dijkstraFalseList(shortestDistances, addedVertices, nVertices )
+
+    shortestDistances[startVertex] = 0
+    let parents = new Array(nVertices)
+    const NO_PARENT = -1
+    parents[startVertex] = NO_PARENT
+    for (let i = 1; i < nVertices; i++) {
+        let nearestVertex = -1
+        let shortestDistance = +Infinity
+        for (let j = 0; j < nVertices; j++) {
+            if (!addedVertices[j] && shortestDistances[j] < shortestDistance) {
+                nearestVertex = j
+                shortestDistance = shortestDistances[j]
+            }
+        }
+        addedVertices[nearestVertex] = true
+        dijkstraUpdateVertices(adjacencyMatrix, shortestDistance, shortestDistances, parents, nearestVertex, nVertices, rowCells)
+    }
+    mkTableResultDijkstra(startVertex, shortestDistances, parents, tablResultID, rowCells)
+}
+
+function mkTableResultDijkstra(startVertex, distances, parents, tablResultID, rowCells) {
+    let tableResult = document.getElementById(tablResultID)
+    let nVertices = distances.length
+    tableResult.insertRow(-1).innerHTML = `<th scope="col">Đỉnh (Vertex)</th><th scope="col">Khoảng cách (Distance)</th><th scope="col">Đường đi (Path)</th><th scope="col">Các bước (Step by step)</th>`
+    for (let i = 0; i < nVertices; i++) {
+        if (i != startVertex) {
+            tableResult.insertRow(-1).innerHTML =
+            `<td>${coverNumToAlpha(startVertex)} -> ${coverNumToAlpha(i)}</td>
+            <td>${distances[i]}</td>
+            <td>${printDijkstraPath(i, parents)}</td>
+            <td>${(startVertex == 0)? rowCells[i -1] : rowCells[i]}</td>`
+        }
+    }
+}
+
+function printDijkstraPath(currentVertex,parents) {
+    const NO_PARENT = -1
+    if (currentVertex == NO_PARENT) {
+        return ''
+    }
+    return (`${printDijkstraPath(parents[currentVertex], parents)} ${coverNumToAlpha(currentVertex)}`)
+}
+
+function coverNumToAlpha(number){
+    const alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K']
+    for (const alphaIndex in alpha) {
+        if (alphaIndex == number) {
+            return alpha[alphaIndex]
+        }
+    }
+}
+
+
---
+++ MatrixCalculator/js/matrixAlgorithm.js
@@ -0,0 +1,87 @@
+//A + B
+function plusAnB(calculation = '+', tableIDA = 'matrixA', tableIDB = 'matrixB', tableResultID = 'resultC') {
+    const tableA = document.getElementById(tableIDA)
+    const tableB = document.getElementById(tableIDB)
+    const resultC = document.getElementById(tableResultID)
+    if(tableA.rows.length == tableB.rows.length){
+        refreshMatrixResult(calculation)
+        for (let i = 0; i < tableA.rows.length; i++) {
+            const rowA = tableA.rows[i]
+            const rowB = tableB.rows[i]
+            let rowCells = ''
+            for (let j = 0; j < rowA.cells.length; j++) {
+                const cellA = Number(rowA.cells[j].firstChild.value)
+                const cellB = Number(rowB.cells[j].firstChild.value)
+                calculation == '+'
+                ? rowCells += `<td><input type="number" class="form-control border-radius-0" value= '${cellA + cellB}'></td>`
+                : rowCells += `<td><input type="number" class="form-control border-radius-0" value= '${cellA - cellB}'></td>`
+            }
+            resultC.insertRow(-1).innerHTML = rowCells
+        }
+    } else {
+        $('#resultHeader').replaceWith('<h5 id="resultHeader" class="text-warning">*Kích cỡ ma trận A phải bằng ma trận B</h5>')
+        hideResult()
+    }
+}
+
+// A times B
+function AtimesB(matrixL = 'matrixA', matrixR = 'matrixB', matrixResult = 'resultC', pow = false) {
+    const tabA = document.getElementById(matrixL)
+    const tabB = document.getElementById(matrixR)
+    const resC = document.getElementById(matrixResult)
+    if (tabA.rows[0].cells.length == tabB.rows.length) {
+        pow ? {} : refreshMatrixResult('*')
+        for (let tabArow of tabA.rows) {
+            let newCellC = ''
+            for (let j = 0; j < tabA.rows[0].cells.length; j++) {
+                let cellC = 0
+                for (let k = 0; k < tabB.rows.length; k++) {
+                    cellC += tabArow.cells[k].firstChild.value * tabB.rows[k].cells[j].firstChild.value
+                }
+                newCellC += `<td><input type="number" class="form-control border-radius-0" value= '${cellC}'></td>`
+            }
+            resC.insertRow(-1).innerHTML = newCellC
+        }
+    } else{
+        $('#resultHeader').replaceWith('<h5 id="resultHeader" class="text-warning">*Số cột của ma trận A phải bằng số hàng của ma trận B</h5>')
+        hideResult()
+    }
+}
+
+//Raiser to the power of value 
+function powMatrix(tableID, powValue, tableResultID){
+    if(powValue < -1){
+        return document.getElementById(tableResultID).insertRow(-1).innerHTML = `<td><p>SORRY, NO TECHNICAL SUPPORT HERE 07</p></td>`
+    } else if (powValue < 2){
+        switch(powValue){
+        case -1:
+            document.getElementById(tableResultID).insertRow(-1).innerHTML = `<td><p>TÌM ĐỊNH THỨC KHÁC 0</p></td>`
+            break
+        case 0:
+            identityMatrix(tableID, 1, tableResultID)
+            break
+        case 1:
+            copyMatrixTo(tableID, tableResultID)
+            break
+        }
+    } else{
+        copyMatrixTo(tableID, 'tempA')
+        powValue = powValue - 2
+        for (let i = 0; i < powValue; i++) {
+            if (i % 2 == 0) {
+                AtimesB('tempA', tableID, 'tempB', true)
+                delTable('tempA')
+            } else {
+                AtimesB('tempB', tableID, 'tempA', true)
+                delTable('tempB')
+            }
+        }
+        if (powValue % 2 == 0) {
+            AtimesB('tempA', tableID, tableResultID, true)
+            delTable('tempA')
+        } else {
+            AtimesB('tempB', tableID, tableResultID, true)
+            delTable('tempB')
+        }
+    }
+}

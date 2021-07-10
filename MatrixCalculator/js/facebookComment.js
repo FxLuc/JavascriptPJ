8c0c430c286ef6f41eb3a6e7296dffcee0cb691f
Flex Alderson <63008853+LocalHost127000@users.noreply.github.com>
2021-07-10 16:49:39 +0700

    Add facebook comment plugin

---
+++ MatrixCalculator/js/facebookComment.js
@@ -0,0 +1,20 @@
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0]
+    if (d.getElementById(id)) {
+        return
+    }
+    js = d.createElement(s)
+    js.id = id
+    js.src = "https://connect.facebook.net/vi_VN/sdk.js"
+    fjs.parentNode.insertBefore(js, fjs)
+}(document, 'script', 'facebook-jssdk')) //IIFE - Immediately Invoked Function Expression
+
+window.fbAsyncInit = () => {
+    FB.init({
+        appId      : 1123915621464064,
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v11.0'
+    })
+    FB.AppEvents.logPageView()
+}
